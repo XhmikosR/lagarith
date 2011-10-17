7e088e3e0b19d8a55bc4b6912a30462831b5a541
XhmikosR <xhmikosr@users.sourceforge.net>
2011-10-17 22:51:09 +0000

    add a script to compile everything
    git-svn-id: https://lagarith.googlecode.com/svn/trunk@9 4df1c2b4-fbc5-7d03-857b-8b221e9b2649

--- installer/build_installer.bat
+++ installer/build_installer.bat
@@ -6,6 +6,33 @@ REM You can set here the Inno Setup path if for example you have Inno Setup Unic
 REM installed and you want to use the ANSI Inno Setup which is in another location
 SET "InnoSetupPath=H:\progs\thirdparty\isetup-5.4.2"
 
+
+CALL :SubGetInnoSetupPath
+
+IF NOT EXIST %InnoSetupPath% (
+  ECHO. & ECHO.
+  ECHO Inno Setup not found!
+  GOTO END
+)
+
+CALL :SubInno
+
+
+:END
+ECHO. & ECHO.
+ENDLOCAL
+EXIT /B
+
+
+:SubInno
+ECHO.
+TITLE Building lagarith installer...
+"%InnoSetupPath%\iscc.exe" /Q "Lagarith_setup.iss"
+IF %ERRORLEVEL% NEQ 0 (ECHO Build failed! & GOTO END) ELSE (ECHO Installer compiled successfully!)
+EXIT /B
+
+
+:SubGetInnoSetupPath
 REM Detect if we are running on 64bit WIN and use Wow6432Node, set the path
 REM of Inno Setup accordingly and compile the installer
 IF "%PROGRAMFILES(x86)%zzz"=="zzz" (
@@ -19,21 +46,6 @@ IF NOT DEFINED InnoSetupPath (
     'REG QUERY "%U_%\Inno Setup 5_is1" /v "Inno Setup: App Path"2^>Nul^|FIND "REG_"') DO (
     SET "InnoSetupPath=%%a" & CALL :SubInnoSetupPath %%InnoSetupPath:*Z=%%)
 )
-
-IF NOT EXIST %InnoSetupPath% (
-  ECHO. & ECHO.
-  ECHO Inno Setup not found!
-  GOTO END
-)
-
-"%InnoSetupPath%\iscc.exe" /Q "Lagarith_setup.iss"
-IF %ERRORLEVEL% NEQ 0 (ECHO Build failed & GOTO END) ELSE (ECHO Installer compiled successfully!)
-
-
- :END
-ECHO. & ECHO.
-ENDLOCAL
-PAUSE
 EXIT /B
 
 
